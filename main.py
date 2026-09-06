c7fabf9a20776a736cde9e45f49dcdc182d1b817
Твоё имя <@email.com>
2026-09-06 12:03:03 +0300

    push and delete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from uuid import uuid4
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
@@ -24,8 +24,9 @@ class TaskCreateShema(BaseModel):
     title: str
 
 
-class BookShema(BaseModel):
-    book: str
+class TaskUpdateShema(BaseModel):
+    title: str | None = None
+    completed: bool | None = None
 
 
 tasks: list[TaskShema] = []
@@ -43,19 +44,19 @@ def create_task(payload: TaskCreateShema) -> TaskShema:
     tasks.append(new_task)
     return new_task
 
+@app.patch("/tasks/{task_id}")
+def update_task(task_id: str, payload: TaskUpdateShema):
+    for task in tasks:
+        if task.id == task_id:
+            if payload.title:
+                task.title = payload.title
+            if payload.completed is not None:
+                task.completed = payload.completed
 
-book = ""
+            return task
 
-
-@app.get("/book")
-def get_book():
-    if book:
-        return {"message": f"Любимая книга: {book}"}
-    return {"message": "Книга не задана"}
-
-
-@app.post("/book")
-def set_book(payload: BookShema):
-    global book
-    book = payload.book
-    return {"message": f"Книга '{book}' сохранена"}
+@app.delete('/tasks/{task_id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(task_id: str):
+    for task in tasks:
+        if task.id == task_id:
+            tasks.remove(task)
