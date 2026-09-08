3c0be890d8998062e65d005c0cbad66583229430
Твоё имя <@email.com>
2026-09-08 21:00:57 +0300

    created db

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from contextlib import asynccontextmanager
 from uuid import uuid4
 
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI, status, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped
-from sqlalchemy.testing.schema import mapped_column
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column, Session
 
 DATABASE_URL = "postgresql+psycopg://postgres:admin@127.0.0.1:15432/postgres"
 engine = create_engine(DATABASE_URL)
@@ -36,7 +35,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
     allow_methods=["*"],
-    allow_headers=["*"]
 )
 
 
@@ -58,41 +56,48 @@ class TaskUpdateShema(BaseModel):
     completed: bool | None = None
 
 
-tasks: list[TaskShema] = []
+def get_db():
+    db = Sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def task_orm_to_model(task_orm: TaskORM) -> TaskShema:
+    return TaskShema(id=task_orm.id, title=task_orm.title, completed=task_orm.completed)
 
 
 @app.get("/tasks")
-def read_tasks() -> list[TaskShema]:
-    return tasks
+def read_tasks(db: Session = Depends(get_db)) -> list[TaskShema]:
+    tasks_from_db = db.scalars(select(TaskORM)).all()
+    return [task_orm_to_model(task) for task in tasks_from_db]
 
 
 @app.post("/tasks", status_code=status.HTTP_201_CREATED)
-def create_task(payload: TaskCreateShema) -> TaskShema:
-    new_task = TaskShema(id=str(uuid4()), title=payload.title, completed=False)
+def create_task(payload: TaskCreateShema, db: Session = Depends(get_db)) -> TaskShema:
+    new_task = TaskORM(title=payload.title, completed=False)
+    db.add(new_task)
+    db.commit()
 
-    tasks.append(new_task)
-    return new_task
+    return task_orm_to_model(new_task)
 
 @app.patch("/tasks/{task_id}")
-def update_task(task_id: str, payload: TaskUpdateShema):
-    for task in tasks:
-        if task.id == task_id:
-            if payload.title:
-                task.title = payload.title
-            if payload.completed is not None:
-                task.completed = payload.completed
-            return task
+def update_task(task_id: str, payload: TaskUpdateShema, db: Session = Depends(get_db)) -> TaskShema:
+    task_for_update = db.get(TaskORM, task_id)
+    if payload.title:
+        task_for_update.title = payload.title
+    if payload.completed:
+        task_for_update.completed = payload.completed
 
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Задача не найдена")
+    db.commit()
+    return task_orm_to_model(task_for_update)
 
 @app.delete('/tasks/{task_id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_task(task_id: str):
-    for task in tasks:
-        if task.id == task_id:
-            tasks.remove(task)
-            return
-
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Задача не найдена")
+def delete_task(task_id: str, db: Session = Depends(get_db)) -> None:
+    task_for_delete = db.get(TaskORM, task_id)
+    db.delete(task_for_delete)
+    db.commit()
 
 
 #КАТЕГОРИИ
